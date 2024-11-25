712a2e66f140a37020cce12bbf3f955b08f0270e
iamkdao <daominhkhoi64@gmail.com>
2024-11-25 13:49:22 -0500

    Added some adjustments

--- app/main/student/forms.py
+++ app/main/student/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField, BooleanField, FloatField
 from wtforms.validators import  ValidationError, DataRequired, Length
-from wtforms_sqlalchemy.fields import QuerySelectMultipleField
+from wtforms_sqlalchemy.fields import QuerySelectMultipleField, QuerySelectField
 from wtforms.widgets import ListWidget, CheckboxInput
 
 from app import db
@@ -16,9 +16,17 @@ class ApplicationForm(FlaskForm):
     year_took_course = StringField('Year course was taken', validators=[DataRequired()])
     term_took_course = SelectField('Term course was taken', choices = ['A', 'B', 'C', 'D', 'E'])
     year_to_apply = StringField('Applying year', validators=[DataRequired()])
-    term_to_apply = SelectField('Applying term', choices = ['A', 'B', 'C', 'D', 'E'])
-    # term_to = QuerySelectMultipleField(
-    #     'Select available term',
-    #     query_factory = lambda : db.session.scalars(sqla.select)
-    #     )
-    submit = SubmitField('Create')
+    term_to_apply = QuerySelectField(
+        'Select available term',
+        query_factory = None,
+        get_label=None
+        )
+    submit = SubmitField('Apply')
+    
+    def __init__(self, position_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Dynamically update the query factory based on the position_id
+        self.term_to_apply.query_factory = lambda: db.session.scalars(
+            sqla.select(Position).where(Position.id == position_id)
+        )
+        self.term_to_apply.get_label = lambda position : position.get_section_term()
--- app/main/student/routes.py
+++ app/main/student/routes.py
@@ -15,24 +15,24 @@ from app.main.student import student_blueprint as bp_student
 @login_required
 @role_required('Student')
 def index():
-
-    return render_template('student_index.html', title="SA Portal")
+    positions = db.session.scalars(sqla.select(Position).where(Position.curr_SA < Position.max_SA))
+    return render_template('student_index.html', title="SA Portal", positions = positions)
     
     
-@bp_student.route('/student/application/<section_id>/<position_id>', methods=['GET', 'POST'])
+@bp_student.route('/student/application/<position_id>', methods=['GET', 'POST'])
 @login_required
-def apply(section_id, position_id):
-    section = db.session.get(Section, section_id)
+def apply( position_id):
+    # section = db.session.get(Section, section_id)
     position = db.session.get(Position, position_id)
     
-    if section is None:
-        flash('Section not found!')
-        return redirect(url_for('main.student.index'))
+    # if section is None:
+    #     flash('Section not found!')
+    #     return redirect(url_for('main.student.index'))
     if position is None:
         flash('Position not found!')
         return redirect(url_for('main.student.index'))
     
-    af = ApplicationForm()
+    af = ApplicationForm(position_id=position_id)
     
     if af.validate_on_submit():
         new_app = Application(
